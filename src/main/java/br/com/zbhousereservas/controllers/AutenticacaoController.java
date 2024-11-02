f634af0ef16d72fc29139bd8cc98c17ec2c8b678
Felipe Coelho <felipeaacoelho.com>
2024-11-02 00:54:57 -0300

    autenticacao; cookie

--- src/main/java/br/com/zbhousereservas/controllers/AutenticacaoController.java
+++ src/main/java/br/com/zbhousereservas/controllers/AutenticacaoController.java
@@ -4,7 +4,10 @@ import br.com.zbhousereservas.dto.DadosAutenticacao;
 import br.com.zbhousereservas.entities.Usuario;
 import br.com.zbhousereservas.security.DadosTokenJWT;
 import br.com.zbhousereservas.security.TokenService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -24,12 +27,25 @@ public class AutenticacaoController {
     private TokenService tokenService;
 
     @PostMapping
-    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
+    public ResponseEntity efetuarLogin(@RequestBody @Valid @NotNull DadosAutenticacao dados, HttpServletResponse response){
         var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(authenticationToken);
+        try{
 
-        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+            var authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+            var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+            // Adicionando o token JWT como um cookie na resposta
+            Cookie cookie = new Cookie("token", tokenJWT);
+            cookie.setSecure(true); // Defina como `true` se estiver usando HTTPS
+            cookie.setHttpOnly(true);
+            cookie.setMaxAge(24 * 60 * 60); // Define a validade do cookie em segundos (neste caso, 1 dia)
+            response.addCookie(cookie);
+
+            return ResponseEntity.ok(dados.login());
+
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Usuário/Senha errados ou ausentes");
+        }
     }
 }
