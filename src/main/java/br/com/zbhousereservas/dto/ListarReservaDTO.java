51662801ce0a5f3c1c301007852de7deea8d29d7
Felipe Coelho <felipeaacoelho.com>
2024-11-03 15:20:06 -0300

    recebendo cookie na requisição

--- src/main/java/br/com/zbhousereservas/dto/ListarReservaDTO.java
+++ src/main/java/br/com/zbhousereservas/dto/ListarReservaDTO.java
@@ -1,15 +1,17 @@
 package br.com.zbhousereservas.dto;
 
 import br.com.zbhousereservas.entities.Reserva;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 
-public record ListarReservaDTO (String nome,
-                                LocalDateTime checkin,
-                                LocalDateTime checkout,
+public record ListarReservaDTO (Long id, String nome,
+                                String documento,
+                                @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss") LocalDateTime checkin,
+                                @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss") LocalDateTime checkout,
                                 Double valor_reserva,
                                 int desconto,
                                 List<Parcelas> pagamentos){
@@ -17,7 +19,8 @@ public record ListarReservaDTO (String nome,
 
     public ListarReservaDTO(@NotNull Reserva reserva){
 
-        this(reserva.getNome(),
+        this(reserva.getId(), reserva.getNome(),
+                reserva.getDocumento(),
                 reserva.getCheckin(),
                 reserva.getCheckout(),
                 reserva.getValor_reserva(),
