bdf56b357cac013ee14ca2c5620a90406789635b
Felipe Coelho <felipeaacoelho@gmail.com>
2024-11-19 13:41:26 -0300

    DTO usuario autenticado

--- src/main/java/br/com/zbhousereservas/controllers/AutenticacaoController.java
+++ src/main/java/br/com/zbhousereservas/controllers/AutenticacaoController.java
@@ -1,8 +1,10 @@
 package br.com.zbhousereservas.controllers;
 
 import br.com.zbhousereservas.dto.DadosAutenticacao;
+import br.com.zbhousereservas.dto.UsuarioAutenticado;
 import br.com.zbhousereservas.entities.Usuario;
 import br.com.zbhousereservas.security.TokenService;
+import br.com.zbhousereservas.services.AutenticacaoService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -11,10 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
@@ -24,6 +23,8 @@ public class AutenticacaoController {
     private AuthenticationManager manager;
     @Autowired
     private TokenService tokenService;
+    @Autowired
+    private AutenticacaoService autenticacaoService;
 
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid @NotNull DadosAutenticacao dados, HttpServletResponse response){
@@ -37,10 +38,23 @@ public class AutenticacaoController {
             String cookieValue = "token=" + tokenJWT + "; HttpOnly; Secure; SameSite=None; Domain=localhost; Path=/; Max-Age=" + (24 * 60 * 60);
             response.addHeader("Set-Cookie", cookieValue);
 
-            return ResponseEntity.ok(dados.login());
+            return ResponseEntity.ok(new UsuarioAutenticado(dados.login()));
 
         }catch (Exception e){
             return ResponseEntity.badRequest().body("Usuário/Senha errados ou ausentes");
         }
+    }
+
+    @GetMapping("/{user}")
+    public ResponseEntity validarUsuario(@PathVariable String user){
+
+        try {
+            var usuario = this.autenticacaoService.loadUserByUsername(user);
+
+            return ResponseEntity.ok().body(usuario);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Usuário inválido");
+        }
+
     }
 }
