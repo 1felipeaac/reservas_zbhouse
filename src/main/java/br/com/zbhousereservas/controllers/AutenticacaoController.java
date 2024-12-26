fdcad214cd8ff4e1d9685600e4e97332023b81be
Felipe Coelho <felipeaacoelho@gmail.com>
2024-12-26 09:04:36 -0300

    Configurando cookie manualmente

--- src/main/java/br/com/zbhousereservas/controllers/AutenticacaoController.java
+++ src/main/java/br/com/zbhousereservas/controllers/AutenticacaoController.java
@@ -5,7 +5,6 @@ import br.com.zbhousereservas.dto.UsuarioAutenticado;
 import br.com.zbhousereservas.entities.Autenticacao;
 import br.com.zbhousereservas.security.TokenService;
 import br.com.zbhousereservas.services.AutenticacaoService;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -42,16 +41,11 @@ public class AutenticacaoController {
 
             var tokenJWT = tokenService.gerarToken((Autenticacao) authentication.getPrincipal());
 
-            int maxAgeInSeconds = 24 * 60 * 60;
-            Cookie cookieValue = new Cookie("token", tokenJWT);
-            cookieValue.setHttpOnly(true);
-            cookieValue.setSecure(true);
-            cookieValue.setDomain(cookieDomain); // Defina o domínio correto do seu cookie
-            cookieValue.setPath("/");
-            cookieValue.setMaxAge(maxAgeInSeconds);
+            String cookie = setCookie(tokenJWT);
 
-            response.addCookie(cookieValue);
-            log.info("Usuario [{}] autenticado. Dominio [{}]. Token [{}]", dados.login(), cookieDomain, tokenJWT);
+            response.setHeader("Set-Cookie", cookie);
+
+            log.info("Usuario [{}] autenticado. Dominio [{}]", dados.login(), cookieDomain);
             return ResponseEntity.ok(new UsuarioAutenticado(dados.login()));
 
 
@@ -63,6 +57,21 @@ public class AutenticacaoController {
             log.error(e.getMessage());
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    private @NotNull String setCookie(String tokenJWT) {
+        int maxAgeInSeconds = 24 * 60 * 60;
+//            Cookie cookieValue = new Cookie("token", tokenJWT);
+//            cookieValue.setHttpOnly(true);
+//            cookieValue.setSecure(true);
+//            cookieValue.setSameSite
+//            cookieValue.setDomain(cookieDomain); // Defina o domínio correto do seu cookie
+//            cookieValue.setPath("/");
+//            cookieValue.setMaxAge(maxAgeInSeconds);
+//            response.addCookie(cookieValue);
+
+        String cookie = "token=" + tokenJWT + "; HttpOnly; Secure; SameSite=None; Domain=" + cookieDomain + "; Path=/; Max-Age=" + maxAgeInSeconds;
+        return cookie;
     }
 
     @GetMapping("/{user}")
